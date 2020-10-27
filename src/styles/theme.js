37614ca580cf89a4d97b22feba59fd0a59e7e9de
ghocevar <hocevar.gregi@gmail.com>
2020-10-27 20:39:38 +0100

    Start refactoring Header component

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,6 +4,12 @@ export default {
     blue: '#268BD2',
     black: '#000',
   },
+  fontFamily: {
+    sans:
+      'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,sans-serif',
+    mono:
+      'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,Bitstream Vera Sans Mono, Courier New, monospace',
+  },
   fontWeights: {
     bold: 700,
   },
