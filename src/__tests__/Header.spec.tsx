4d446c6fea1948ed62af50160e77847655013ea6
Gregor Hocevar <hocevar.gregi@gmail.com>
2022-07-04 11:01:56 +0000

    Add 2 basic tests for Header and Home components

---
+++ src/__tests__/Header.spec.tsx
@@ -0,0 +1,12 @@
+import { cleanup, render } from '@testing-library/react'
+import Header from '../components/Header';
+import '@testing-library/jest-dom'
+
+beforeEach(cleanup)
+
+describe('Header', () => {
+	it('renders a title', async () => {
+		const {getByText} = render(<Header />);
+		expect(getByText('Jobs')).toBeVisible();
+	});
+});
