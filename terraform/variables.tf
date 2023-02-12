46809dc933d9ab6b5703185c99601465105c1022
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2023-02-12 20:29:07 +0100

    Add terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_credentials" {
+  type        = string
+  description = "The GCP credentials to use for the provider"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project id of the project"
+}
+
+variable "project_number" {
+  type        = string
+  description = "The project number of the project"
+}
