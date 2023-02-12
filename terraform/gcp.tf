ce4834baee4c76a74784819486054c08d710898b
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2023-02-12 21:44:43 +0100

    Add Cloud Run

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -4,3 +4,33 @@ resource "google_artifact_registry_repository" "repository" {
   description   = "Artifact Registry repository for hackernews-jobs project"
   format        = "DOCKER"
 }
+
+resource "google_cloud_run_service" "main" {
+  name     = "hackernews-jobs-api-main"
+  location = "europe-west8"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.main.location
+  project     = google_cloud_run_service.main.project
+  service     = google_cloud_run_service.main.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
